2f3f24186455d8cf1bbcdcf74d9c63958bdd5093
hyunmin <khm010809@naver.com>
2024-08-01 18:15:59 +0900

    feat: 정보값 detail view에 넣어줌

---
+++ app/src/main/java/com/example/toiletfinder/ToiletDetailView.kt
@@ -0,0 +1,23 @@
+package com.example.toiletfinder
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ToiletDetailView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_toilet_detail_view)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val toiletInfo = intent.getParcelableExtra<ToiletInfo>("toilet_info")
+
+    }
+}
